8757ea32b8210f585422254340236626e145a9c1
ranim20-meet <ranim20@meet.mit.edu>
2018-08-13 07:29:48 -0400

    Stopping here

--- JunkJourney.py
+++ JunkJourney.py
@@ -1,3 +1,4 @@
+
 from PIL import Image
 import turtle
 import random
@@ -7,22 +8,26 @@ screen = turtle.Screen()
 screen_x = 700
 screen_y = 1050
 screen.setup(screen_x, screen_y)
-'''
+
+
 bg = Image.open('background.jpeg')
 pix=bg.load()
 bagr=bg.resize((700,1050),Image.ANTIALIAS)
 bagr.save('thebackground.gif')
 turtle.bgpic = ('thebackground.gif')
-'''
+
 player = turtle.Turtle()
 pl = Image.open('papernobg.gif')
-pn=pl.resize((50,50),Image.ANTIALIAS)
-turtle.register_shape(pn)
-player.shape(pn)
+pn=pl.resize((30,30),Image.ANTIALIAS)
+pn.save('player.gif')
+turtle.register_shape('player.gif')
+
+screen.bgpic = ('thebackground.gif')
+player.shape('player.gif')
 
 
 player.penup()
-player.goto(0,480)
+player.goto(0,450)
 
 def move_left():
     x,y = player.pos()
@@ -34,13 +39,49 @@ def move_right():
 
 def move_down():
     x,y = player.pos()
-    player.goto(x,y-10)
+    player.goto(x,y-50)
+i = 1
+paper_list = []
 
+def make_trash():
+    global i
+    global paper_list
+    paper = player.clone() #creates a clone of the player
+    paper.shape('player.gif') #setting it's shape to paper
+    paper.speed(10)
+    paper.penup() 
+    paper.ht() #hide turtle
+    paper.goto(random.randint(-15,15)*20,-400) #the x postition must be a
+                                               #multiply of 20
+    paper.speed(3)
+    paper.st()#show turtle
+    paper.left(90)
+    paper_list.append(paper)
+    i+=1
+    print(paper_list)
+    move_trash()
+    make_trash()
+
+def move_trash():
+    for j in paper_list:  # for moving multiple papers
+        x,y = j.pos()
+        print(j.pos())
+        while y < 500:  # while the paper is inside the screen
+            j.forward(50)
+            x,y = j.pos()
+            print("poses: ",player.pos(),j.pos())
+            if player.pos() == j.pos(): 
+                j.ht() # make the paper disappear
+        print("finish")
+        j.ht() #make the paper disappear even if it was not colected
+        
+        
+#turtle.onkeypress(move_trash, "p")
+turtle.onkeypress(make_trash, " ")
 turtle.onkeypress(move_left, "Left")
 turtle.onkeypress(move_right, "Right")
 turtle.onkeypress(move_down, "Down")
 turtle.listen()
-
 
 
 turtle.mainloop()
